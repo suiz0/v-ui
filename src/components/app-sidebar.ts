48277088aaa8e7b7d547bdce9640779870c4e1b9
Cesar Iduarte <ciduarte@gmail.com>
2020-06-22 13:33:13 -0500

    Enhance sidebar add collapse option

--- src/components/app-sidebar.ts
+++ src/components/app-sidebar.ts
@@ -2,15 +2,38 @@ declare let Vue: any;
 
 const Sidebar = Vue.component('app-sidebar', {
     template: `
-        <div class="sidebar sidebar--expanded" :class="['bg-' + variant, 'text-light']">
+        <div class="sidebar" :class="['bg-' + variant, 'text-light', CSSStatus]">
             <h3 class="ml-3" v-show="title.length>0">{{title}}</h3>
-            <slot>
-                Content here
-            </slot>
+            <section class="sidebar__content" v-show="isExpanded">
+                <slot>
+                    Content here
+                </slot>
+            </section>
+            <section class="sidebar__content" v-show="!isExpanded">
+                <slot name="collapsed">
+                &lt;C&gt;
+                </slot>
+            </section>
         </div>`,
     props: {
         title: {type: String, default: ""},
-        variant: {type: String, default: "dark"}
+        variant: {type: String, default: "dark"},
+        isCollapsed: {type: Boolean, default: false}
+    },
+    data () {
+        return {
+            isExpanded: !this.isCollapsed
+        }
+    },
+    watch: {
+        isCollapsed(val) {
+            this.isExpanded = !val;
+        }
+    },
+    computed: {
+        CSSStatus() {
+            return this.isExpanded ? "sidebar--expanded" : "";
+        }
     }
 });
 
