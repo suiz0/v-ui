e1ff48b7c37cc56e0aa10b702b7cd8b2027ae706
cesar iduarte <ciduarte@gmail.com>
2021-04-03 11:10:42 -0600

    Add required property to app-input control

--- src/components/app-input.ts
+++ src/components/app-input.ts
@@ -1,12 +1,13 @@
 import Vue from 'vue';
 
 const AppInput = Vue.component('app-input', {
-    template: `<b-form-group :label="label">
-        <b-form-input :type="type" :state="isValid" :size="size" v-model="value"></b-form-input>
+    template: `<b-form-group :label="label" :class="{'form-group--required': isRequired}">
+        <b-form-input :type="type" :required="isRequired ? 'required': null" :state="isValid" :size="size" v-model="value"></b-form-input>
         <b-form-invalid-feedback :state="isValid">{{error}}</b-form-invalid-feedback>
         </b-form-group>`,
     props: {
         isValid: {type: Boolean, default: null},
+        isRequired: {type: Boolean, default: false},
         error: {type:String, default: ''},
         value: {type: Object},
         size: {type: String, default: 'sm'},
