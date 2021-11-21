cad85bdd39c87bf07a24113cf6859c9f9108e397
cesar iduarte <ciduarte@gmail.com>
2021-11-20 23:20:11 -0600

    fix: invalidate forms fields on submission

--- src/components/form-ctrls/app-form.ts
+++ src/components/form-ctrls/app-form.ts
@@ -12,7 +12,7 @@ const AppForm = Vue.component('app-form', {
         },
         submit(silent = false) {
             let ret = false;
-            if(this.$refs['frm'].checkValidity() === true) {
+            if (this.validateFields(this.$refs['frm'].children)) {
                 ret = true;
                 if(!silent) this.$emit('submitted');
             } else {
@@ -20,6 +20,21 @@ const AppForm = Vue.component('app-form', {
             }
 
             if(silent) return ret;
+        },
+        validateFields(nodes) {
+            let isOk = true;
+            for (const node of nodes) {
+                if (node.tagName.toLowerCase() === 'input') {
+                    if (!node.checkValidity()) {
+                        node.classList.add('is-invalid');
+                        return false;
+                    }
+                } else if (node.children.length) {
+                    return this.validateFields(node.children);
+                }
+            }
+
+            return isOk;
         }
     }
 });
--- src/components/form-ctrls/app-input.ts
+++ src/components/form-ctrls/app-input.ts
@@ -2,13 +2,13 @@ import Vue from 'vue';
 
 const AppInput = Vue.component('app-input', {
     template: `<b-form-group :label="label" :class="{'form-group--required': isRequired}">
-        <b-form-input :type="type" :required="isRequired ? 'required': null" :state="isValid" :size="size" v-model="value"></b-form-input>
-        <b-form-invalid-feedback :state="isValid">{{error}}</b-form-invalid-feedback>
+        <b-form-input ref="input" @change="resetValidation" :type="type" :state="isValid" :required="isRequired ? 'required': null" :state="isValid" :size="size" v-model="value"></b-form-input>
+        <b-form-invalid-feedback>{{error}}</b-form-invalid-feedback>
         </b-form-group>`,
     props: {
         isValid: {type: Boolean, default: null},
         isRequired: {type: Boolean, default: false},
-        error: {type:String, default: ''},
+        error: {type:String, default: 'Field with error'},
         value: {type: Object},
         size: {type: String, default: 'sm'},
         type: {type: String, default: () => { return 'text'}},
@@ -17,6 +17,11 @@ const AppInput = Vue.component('app-input', {
     watch: {
         value: function() {
             this.$emit('input', this.value);
+        }
+    },
+    methods: {
+        resetValidation() {
+            this.$refs['input'].$el.classList.remove('is-invalid');
         }
     }
 });
