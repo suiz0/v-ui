0c7e0d7b114e12ee50d3c1554b3462024f47d555
cesar iduarte <ciduarte@gmail.com>
2021-12-16 20:51:52 -0600

    fix: adjust header

--- src/components/master-details/app-master-details.ts
+++ src/components/master-details/app-master-details.ts
@@ -3,8 +3,11 @@ import './app-master-details.css';
 
 const AppMasterDetails = Vue.component('app-master-details',{
     template: `<div class="app-master-details">
-        <section class="app-master-details__header d-flex">
-            <h3>{{title}}</h3>
+        <section class="app-master-details__header">
+            <section>
+                <h3>{{title}}</h3>
+                <p v-if="description" class="text-muted">{{description}}</p>
+            </section>
             <div class="d-flex ml-auto mr-1" role="group" aria-label="Basic example">
                 <button type="button" class="btn ml-1 btn-outline-dark"><span class="oi oi-magnifying-glass" @click="search"></span></button>
                 <button type="button" class="btn ml-1 btn-outline-dark" @click="add"> + </button>
@@ -15,6 +18,7 @@ const AppMasterDetails = Vue.component('app-master-details',{
     </div>`,
     props: {
         title: {type: String, default: 'Data Sample'},
+        description: {type: String, default: ''},
         master: {type: Array, default: () =>  []},
         variant: {type: String, default: 'light'},
     },
